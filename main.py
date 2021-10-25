9312f2c07f74e6687c79ece7415490c9c8b9c0fb
Annas <61279556+annasajkh@users.noreply.github.com>
2021-10-26 06:03:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import streamlit as st
 from aitextgen import aitextgen
 
 os.system("gdown --id 1LMYHKntH9b348BviVwEG_CENXPlDDQDO")
-os.system("sudo apt install abcmidi")
+os.system("apt install abcmidi")
 
 st.title("PianoGPT")
 
