8ff82695af90d1e5202a7004a7b49a631755d808
Annas <61279556+annasajkh@users.noreply.github.com>
2021-10-26 18:56:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import streamlit as st
 from scipy.io import wavfile
 from aitextgen import aitextgen
 
-@st.cache(hash_funcs={aitextgen: id})
+@st.cache(hash_funcs={aitextgen: id}, allow_output_mutation=True)
 def setup_ai():
     os.system("gdown --id 1LMYHKntH9b348BviVwEG_CENXPlDDQDO")
     return aitextgen(model_folder=".")
