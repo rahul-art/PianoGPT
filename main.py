7cc7d0e40dbd6bafdebf6b83f6b83e1ad3022f3d
Annas <61279556+annasajkh@users.noreply.github.com>
2021-10-27 21:04:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ if generate:
     
     with st.spinner("Generating..."):
         while True:
-            process = subprocess.Popen([f"cd PianoGPT && ./gpt2tc -m 117M -l 1024 -t 0.8 g X:{random_number}\nT:{title}"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            os.chdir("PianoGPT")
+            process = subprocess.Popen([f"./gpt2tc -m 117M -l 1024 -t 0.8 g X:{random_number}\nT:{title}"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
             result = b""
             st.text(result)
